8d467f5e65d586378d6c3be67e7603412d261960
‪SAGI <57183982+sagagaga4@users.noreply.github.com>
2025-09-01 03:09:39 +0300

    connectedCallBack fix

--- public/components/AccountPage.js
+++ public/components/AccountPage.js
@@ -1,7 +1,5 @@
-import API from "../services/API.js";
-import { MovieItemComponent } from "./MovieItem.js";
 export class AccountPage extends HTMLElement{
-    connectedCallBack(){
+    connectedCallback(){
     const template = document.getElementById("template-account");
     const content = template.content.cloneNode(true);
     this.appendChild(content);

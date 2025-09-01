989170097623ea6e181b0f59007b53ad6db92286
‪SAGI <57183982+sagagaga4@users.noreply.github.com>
2025-09-01 03:06:07 +0300

    replacing jwt instead of token

--- public/services/Store.js
+++ public/services/Store.js
@@ -12,9 +12,13 @@ if(localStorage.getItem("jwt")){
 
 const proxiedStore = new Proxy(Store, {
     set: (target, prop, value ) =>  {
-        if(prop == "jwt"){
-            target[prop] = value;
+        if(prop === "jwt"){
+            target[prop] = value; 
+            if(value === null){
+                localStorage.removeItem("jwt");
+            }else{
             localStorage.setItem("jwt", value);
+        }
         } 
         return true;
     }

1a267c3b83b21df87ddd62bbac93572560ad11a0
‪Sagi <57183982+sagagaga4@users.noreply.github.com>
2025-09-27 01:57:11 +0300

    added 3 endpoints fav/watch/saveToCollec

--- public/services/API.js
+++ public/services/API.js
@@ -1,5 +1,8 @@
 export const API = {
     baseURL: '/api/',
+    getJWT: () => {
+        return localStorage.getItem('jwt');
+    },
     getTopMovies: async () => {
         return await API.fetch("movies/top");
     },
@@ -19,35 +22,62 @@ export const API = {
         const genres = await API.getGenres("genres");
     },
     register: async (name, email, password) => {
-        return await API.send("account/register/",{name,email,password});
+        return await API.send("account/register/",{name, email, password});
     },
+    login: async(email, password) => {
+        return await API.send("account/authenticate/", {email, password})
+    },
+    authenticate: async(email, password) => {
+        return await API.send("account/authenticate/", {email, password})
+    },
+    /*
     authenticate: async (email, password) => {
         return await API.send("account/authenticate/", {email, password})
-    },   
-    send: async (ServiceName, data) => {
-        try{
-            const response = await fetch(API.baseURL + ServiceName, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            });
-            const result = await response.json();
-            return result;
-            }catch(e){
-            console.error(e);
-            }
     },
+    */
+
+    getFavorites: async () => {
+            return await API.fetch("account/favorites/");
+    },     
+    
+    getWatchlist: async () => {
+            return await API.fetch("account/watchlist/");
+    },     
+
+    saveToCollection: async (movie_id, collection) => {
+        return await API.send("account/save-to-collection/", {
+            movie_id, collection
+        });
+    },
+
+    send: async (serviceName, args) => {
+        const jwt = API.getJWT();
+        const response = await fetch(API.baseURL + serviceName, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": jwt ? `Bearer ${jwt}` : null
+            },
+            body: JSON.stringify(args)
+        });
+        const result = await response.json();
+        return result;
+    },
+
     fetch: async (service, args) => {
-        try {
-            const queryString = args ? new URLSearchParams(args).toString() : "";
-            const response = await fetch(API.baseURL + service + '?' + queryString);
-            const result = await response.json();
-            return result;
-        } catch (e) {
-            console.error(e);
-            app.showError();
+        try{
+        const jwt = API.getJWT();
+        const queryString = args ? new URLSearchParams(args).toString() : "";
+        const response = await fetch(API.baseURL + service + '?' + queryString, {
+            headers: {
+                "Authorization": jwt ? `Bearer ${jwt}` : null
+            }
+        });
+        const result = await response.json();
+        return result;
+    } catch (e) {
+        console.error(e);
+        throw e;
         }
     }
 }

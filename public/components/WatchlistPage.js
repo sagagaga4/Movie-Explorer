652218c5a418d347fbab6145307dfbfa607c1beb
‪Sagi <57183982+sagagaga4@users.noreply.github.com>
2025-09-27 01:56:16 +0300

    Using polymorphism for watchlist and favorites

---
+++ public/components/WatchlistPage.js
@@ -0,0 +1,11 @@
+import { API } from "../services/API.js";
+import { CollectionPage } from "./CollectionPage.js";
+
+export default class WatchlistPage extends CollectionPage {
+
+    constructor() {
+        super(API.getWatchlist, "Movie Watchlist")
+    }
+
+}
+customElements.define("watchlist-page", WatchlistPage);

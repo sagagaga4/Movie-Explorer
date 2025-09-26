8658d108809076ece11facc4834c0c297b0bacff
‪Sagi <57183982+sagagaga4@users.noreply.github.com>
2025-09-27 01:58:22 +0300

    added 3 routes for fav/watch/saveToCollec

--- public/services/Routes.js
+++ public/services/Routes.js
@@ -4,6 +4,8 @@ import { MoviesPage } from "../components/MoviesPage.js"
 import { RegisterPage } from '../components/RegisterPage.js'
 import { LoginPage } from '../components/LoginPage.js'
 import { AccountPage } from "../components/AccountPage.js"
+import FavoritePage  from "../components/FavoritePage.js"
+import WatchlistPage   from "../components/WatchlistPage.js"
 export const routes = [
     {
         path: "/",
@@ -30,4 +32,14 @@ export const routes = [
         component: AccountPage,
         loggedIn: true
     },
+    {
+        path: "/account/favorites",
+        component: FavoritePage,
+        loggedIn: true
+    },    
+    {
+        path: "/account/watchlist",
+        component: WatchlistPage,
+        loggedIn: true
+    }, 
 ]

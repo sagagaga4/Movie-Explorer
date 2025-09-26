64ac6805103b9490f8de5d8c31bb6f85d592d4c2
‪Sagi <57183982+sagagaga4@users.noreply.github.com>
2025-09-27 01:45:23 +0300

    changed SaveCollection return type to bool

--- data/interface.go
+++ data/interface.go
@@ -16,5 +16,5 @@ type AccountStorage interface {
 	Authenticate(string, string) (*models.User, error)
 	Register(string, string, string) (bool, error)
 	GetAccountDetails(string) (models.User, error)
-	SaveCollection(models.User, int, string) (models.User, error)
+	SaveCollection(models.User, int, string) (bool, error)
 }
